d1acb47451cc38b2828e2b5b8d9f7052a8349a0d
xbrooke <554719247@qq.com>
2025-06-11 14:49:18 +0800

    更新

--- jest.setup.js
+++ jest.setup.js
@@ -2,11 +2,15 @@
 /// <reference types="node" />
 /// <reference types="jest" />
 /* eslint-env node, jest */
+/* global global */
 
 import '@testing-library/jest-dom';
 import { TextEncoder, TextDecoder } from 'util';
 
+// @ts-expect-error Node.js global in browser context
 global.TextEncoder = TextEncoder;
+// @ts-expect-error Node.js global in browser context
 global.TextDecoder = TextDecoder;
+
 
 
--- src/jest.setup.js
+++ src/jest.setup.js
@@ -2,13 +2,15 @@
 /// <reference types="node" />
 /// <reference types="jest" />
 /* eslint-env node, jest */
+/* global global */
 
 import '@testing-library/jest-dom';
+import { jest } from '@jest/globals';
 import { TextEncoder, TextDecoder } from 'util';
 
-// @ts-ignore
+// @ts-expect-error Node.js global in browser context
 global.TextEncoder = TextEncoder;
-// @ts-ignore 
+// @ts-expect-error Node.js global in browser context
 global.TextDecoder = TextDecoder;
 
 
@@ -16,28 +18,28 @@ global.TextDecoder = TextDecoder;
 // Mock matchMedia with full MediaQueryList implementation
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
-  // @ts-ignore
+  // @ts-expect-error Mock implementation differs from actual type
   value: jest.fn().mockImplementation(query => {
     const mql = {
       matches: query.includes('dark'), // mock prefers dark mode
       media: query,
       onchange: null,
-      // @ts-ignore
+      // @ts-expect-error Deprecated API but needed for testing
       addListener: jest.fn(), // deprecated but still used in some libs
-      // @ts-ignore
+      // @ts-expect-error Deprecated API but needed for testing
       removeListener: jest.fn(), // deprecated
-      // @ts-ignore
+      // @ts-expect-error Custom mock implementation
       addEventListener: jest.fn((type, listener) => {
         // Store the listener for testing purposes
         mql._listener = listener;
       }),
-      // @ts-ignore
+      // @ts-expect-error Custom mock implementation
       removeEventListener: jest.fn((type, listener) => {
         if (mql._listener === listener) {
           mql._listener = undefined;
         }
       }),
-      // @ts-ignore
+      // @ts-expect-error Custom mock implementation
       dispatchEvent: jest.fn((event) => {
         if (mql._listener && event.type === 'change') {
           mql._listener(event);
