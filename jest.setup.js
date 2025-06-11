d52a62bbcda289b3e257397b1b4bef044d5f2024
xbrooke <554719247@qq.com>
2025-06-11 12:22:57 +0800

    更新1.3

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,10 @@
 // @ts-check
 /// <reference types="node" />
+/// <reference types="jest" />
+/* eslint-env node, jest */
+/* global global, TextEncoder, TextDecoder, jest */
+
+/// <reference types="node" />
 /* eslint-env node, jest */
 
 import '@testing-library/jest-dom';
--- src/jest.setup.js
+++ src/jest.setup.js
@@ -1,10 +1,51 @@
 // @ts-check
 /// <reference types="node" />
+/// <reference types="jest" />
 /* eslint-env node, jest */
-/* global global */
+/* global global, TextEncoder, TextDecoder, jest */
+
+/* eslint-env node, jest */
 
 import '@testing-library/jest-dom';
 import { TextEncoder, TextDecoder } from 'util';
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
+// Mock matchMedia
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+// Mock localStorage
+const localStorageMock = (function() {
+  let store = {};
+  return {
+    getItem(key) {
+      return store[key] || null;
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    removeItem(key) {
+      delete store[key];
+    },
+    clear() {
+      store = {};
+    }
+  };
+})();
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock
+});
