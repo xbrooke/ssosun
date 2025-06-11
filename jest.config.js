86b877e64a2f57f4f645389cad56cc492722e841
xbrooke <554719247@qq.com>
2025-06-11 11:54:53 +0800

    更新1.0

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+export default {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
