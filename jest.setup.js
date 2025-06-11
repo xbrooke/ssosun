fcabb6ba7ce875a0db099e3096d1b73e6e573486
xbrooke <554719247@qq.com>
2025-06-11 12:31:08 +0800

    更新1.5

--- jest.setup.js
+++ jest.setup.js
@@ -2,10 +2,6 @@
 /// <reference types="node" />
 /// <reference types="jest" />
 /* eslint-env node, jest */
-/* global global, TextEncoder, TextDecoder, jest */
-
-/// <reference types="node" />
-/* eslint-env node, jest */
 
 import '@testing-library/jest-dom';
 import { TextEncoder, TextDecoder } from 'util';
--- src/jest.setup.js
+++ src/jest.setup.js
@@ -2,36 +2,51 @@
 /// <reference types="node" />
 /// <reference types="jest" />
 /* eslint-env node, jest */
-/* global global, TextEncoder, TextDecoder, jest */
-
-/* eslint-env node, jest */
 
 import '@testing-library/jest-dom';
 import { TextEncoder, TextDecoder } from 'util';
 
+// @ts-ignore
 global.TextEncoder = TextEncoder;
+// @ts-ignore 
 global.TextDecoder = TextDecoder;
+
+
 
 // Mock matchMedia with full MediaQueryList implementation
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: query.includes('dark'), // mock prefers dark mode
-    media: query,
-    onchange: null,
-    addListener: jest.fn(),
-    removeListener: jest.fn(),
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-    // Additional methods for modern browsers
-    addListener: jest.fn(),
-    removeListener: jest.fn(),
-    // Support for modern event listeners
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
+  // @ts-ignore
+  value: jest.fn().mockImplementation(query => {
+    const mql = {
+      matches: query.includes('dark'), // mock prefers dark mode
+      media: query,
+      onchange: null,
+      // @ts-ignore
+      addListener: jest.fn(), // deprecated but still used in some libs
+      // @ts-ignore
+      removeListener: jest.fn(), // deprecated
+      // @ts-ignore
+      addEventListener: jest.fn((type, listener) => {
+        // Store the listener for testing purposes
+        mql._listener = listener;
+      }),
+      // @ts-ignore
+      removeEventListener: jest.fn((type, listener) => {
+        if (mql._listener === listener) {
+          mql._listener = undefined;
+        }
+      }),
+      // @ts-ignore
+      dispatchEvent: jest.fn((event) => {
+        if (mql._listener && event.type === 'change') {
+          mql._listener(event);
+        }
+        return true;
+      }),
+    };
+    return mql;
+  }),
 });
 
 // Mock localStorage
